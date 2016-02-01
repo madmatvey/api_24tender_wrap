cefaa75404b0013d53206e5b384487a13ff9c44b
madmatvey <pothein@gmail.com>
2016-02-01 19:02:50 +0400

    right encoding input

--- app/controllers/nokogiri_controller.rb
+++ app/controllers/nokogiri_controller.rb
@@ -2,7 +2,8 @@ class NokogiriController < ApplicationController
   def index
     # Load the xml file into a String
     require 'open-uri'
-    doc = Nokogiri.XML(open("http://24tender.ru/api/trades/search?modifiedFrom=2013-01-01T00:00&modifiedTo=2016-12-01T00:00"), nil, 'CP1251')
+   
+    doc = Nokogiri.XML(open("http://24tender.ru/api/trades/search?modifiedFrom=2013-01-01T00:00&modifiedTo=2016-12-01T00:00", 'r:windows-1251').read) #, nil, 'CP1251')
     
     results = doc.to_html
     

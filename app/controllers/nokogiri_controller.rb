066d337078ecd01a70d20d2da59a82b6941332db
madmatvey <potehin@gmail.com>
2016-02-01 23:36:37 +0400

    add sequel gem

--- app/controllers/nokogiri_controller.rb
+++ app/controllers/nokogiri_controller.rb
@@ -1,10 +1,11 @@
 class NokogiriController < ApplicationController
   def index
-    # Load the xml file into a String
     require 'open-uri'
-   
-    doc = Nokogiri.XML(open("http://24tender.ru/api/trades/search?modifiedFrom=2013-01-01T00:00&modifiedTo=2016-12-01T00:00")) #, nil, "windows-1251")
+    require 'sequel'
+
+    doc = Nokogiri.XML(open("http://24tender.ru/api/trades/search?modifiedFrom=2013-01-01T00:00&modifiedTo="+Time.now.xmlschema)) #, nil, "windows-1251")
     doc.encoding = 'utf-8'
+    
     results = doc.to_html
     
     # render the output as plain text page

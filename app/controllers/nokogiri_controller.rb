fe7087077e62d464a3e9d8c281a4762c0aef94cb
madmatvey <pothein@gmail.com>
2016-02-01 18:30:28 +0400

    first commit

---
+++ app/controllers/nokogiri_controller.rb
@@ -0,0 +1,12 @@
+class NokogiriController < ApplicationController
+  def index
+    # Load the xml file into a String
+    require 'open-uri'
+    doc = Nokogiri.XML(open("http://24tender.ru/api/trades/search?modifiedFrom=2013-01-01T00:00&modifiedTo=2016-12-01T00:00"), nil, 'CP1251')
+    
+    results = doc.to_html
+    
+    # render the output as plain text page
+    render :text => results, :content_type => 'text/plain'
+  end
+end
